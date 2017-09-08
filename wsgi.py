060f7b47a246b429d2e0dc6b7c030e6824552f0a
stevetsa <mylagimail2004@yahoo.com>
2017-09-07 22:16:42 -0400

    static ip 0.0.0.0

--- wsgi.py
+++ wsgi.py
@@ -6,6 +6,7 @@ if __name__ == '__main__':
     #port = 5000 + random.randint(0, 999)
     #url = "http://127.0.0.1:{0}".format(port)
     port=8000
-    url = "http://127.0.0.1:{0}".format(port)
+    #url = "http://127.0.0.1:{0}".format(port)
+    url = "http://0.0.0.0:{0}".format(port)
     threading.Timer(1.25, lambda: webbrowser.open(url)).start()
-    application.run(port=port, debug=False)
+    application.run(host='0.0.0.0', port=port, debug=False)
